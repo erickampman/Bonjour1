0bc40fee554aeaf69aab29fb5ccd1813496499a1
Eric Kampman <erickampman@yahoo.com>
2021-05-12 06:18:29 -0700

    Initial Commit

---
+++ Bonjour1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bonjour1
+//
+//  Created by Eric Kampman on 5/12/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Bonjour1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bonjour1
+//
+//  Created by Eric Kampman on 5/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
